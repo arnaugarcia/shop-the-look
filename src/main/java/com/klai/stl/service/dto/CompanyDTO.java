306d2121d0ff7e0d0086343a16778e8ec23a7459
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-06-26 20:07:51 +0200

    Fixed equals and hashcode for CompanyDTO #11

--- src/main/java/com/klai/stl/service/dto/CompanyDTO.java
+++ src/main/java/com/klai/stl/service/dto/CompanyDTO.java
@@ -4,15 +4,16 @@ import com.klai.stl.domain.enumeration.CompanyIndustry;
 import com.klai.stl.domain.enumeration.CompanySize;
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
-import javax.validation.constraints.NotNull;
-import lombok.*;
+import javax.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 /**
  * A DTO for the {@link com.klai.stl.domain.Company} entity.
  */
-@EqualsAndHashCode
-@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -98,5 +99,41 @@ public class CompanyDTO implements Serializable {
 
     public void setSubscriptionPlan(SubscriptionPlanDTO subscriptionPlan) {
         this.subscriptionPlan = subscriptionPlan;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof CompanyDTO)) {
+            return false;
+        }
+
+        CompanyDTO companyDTO = (CompanyDTO) o;
+        if (this.id == null) {
+            return false;
+        }
+        return Objects.equals(this.id, companyDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id);
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "CompanyDTO{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", cif='" + getCif() + "'" +
+            ", token='" + getToken() + "'" +
+            ", industry='" + getIndustry() + "'" +
+            ", companySize='" + getCompanySize() + "'" +
+            ", users=" + getUsers() +
+            ", subscriptionPlan=" + getSubscriptionPlan() +
+            "}";
     }
 }
