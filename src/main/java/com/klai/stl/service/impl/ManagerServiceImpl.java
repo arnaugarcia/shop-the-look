dc3754a7652764c370c208cce21486c9eb07eea2
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-06-29 14:42:47 +0200

    Added company and user service dependencies

--- src/main/java/com/klai/stl/service/impl/ManagerServiceImpl.java
+++ src/main/java/com/klai/stl/service/impl/ManagerServiceImpl.java
@@ -1,14 +1,31 @@
 package com.klai.stl.service.impl;
 
+import com.klai.stl.domain.User;
+import com.klai.stl.service.CompanyService;
 import com.klai.stl.service.ManagerService;
+import com.klai.stl.service.UserService;
 import com.klai.stl.service.dto.UserDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ManagerServiceImpl implements ManagerService {
 
+    private final UserService userService;
+    private final CompanyService companyService;
+
+    public ManagerServiceImpl(UserService userService, CompanyService companyService) {
+        this.userService = userService;
+        this.companyService = companyService;
+    }
+
     @Override
     public UserDTO createUser(UserDTO userDTO) {
+        final User user = findCurrentUser();
+        //companyService.
         return null;
+    }
+
+    private User findCurrentUser() {
+        return this.userService.getUserWithAuthorities().orElseThrow();
     }
 }
