34137ef722b8ed04d00e9910ee0588126771302d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-06-29 14:18:23 +0200

    Added new attributes to UserDTO

--- src/main/java/com/klai/stl/service/dto/UserDTO.java
+++ src/main/java/com/klai/stl/service/dto/UserDTO.java
@@ -1,48 +1,44 @@
 package com.klai.stl.service.dto;
 
 import com.klai.stl.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * A DTO representing a user, with only the public attributes.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class UserDTO {
 
     private Long id;
 
     private String login;
 
-    public UserDTO() {
-        // Empty constructor needed for Jackson.
-    }
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String imageUrl;
+
+    private boolean activated = false;
+
+    private String langKey;
 
     public UserDTO(User user) {
         this.id = user.getId();
-        // Customize it here if you need, or not, firstName/lastName/etc
         this.login = user.getLogin();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-            "id='" + id + '\'' +
-            ", login='" + login + '\'' +
-            "}";
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.imageUrl = user.getImageUrl();
+        this.activated = user.isActivated();
+        this.langKey = user.getLangKey();
     }
 }
