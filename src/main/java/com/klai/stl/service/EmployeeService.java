e1a52d576e4a46eecfe22ece8a5fba9c5a9d3851
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-07-04 18:13:35 +0200

    Added documentation for employee service #16

--- src/main/java/com/klai/stl/service/EmployeeService.java
+++ src/main/java/com/klai/stl/service/EmployeeService.java
@@ -5,7 +5,17 @@ import com.klai.stl.service.dto.requests.NewEmployeeRequestDTO;
 import com.klai.stl.service.dto.requests.UpdateEmployeeRequestDTO;
 
 public interface EmployeeService {
+    /**
+     * Creates an employee for the desired company. If the user is an admin it MUST inform the "companyReference" attribute
+     * @param employeeRequest the employee request to create
+     * @return the new user
+     */
     User createEmployee(NewEmployeeRequestDTO employeeRequest);
 
+    /**
+     * Updates an employee for the desired company
+     * @param employeeRequest the employee request to update
+     * @return the updated user
+     */
     User updateEmployee(UpdateEmployeeRequestDTO employeeRequest, String login);
 }
