013b332ef36e72a63bcb63fd0ea7d8d27f66f2d3
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-07-11 16:59:01 +0200

    Added endpoints for billing address #22

--- src/main/java/com/klai/stl/web/rest/BillingAddressResource.java
+++ src/main/java/com/klai/stl/web/rest/BillingAddressResource.java
@@ -1,5 +1,6 @@
 package com.klai.stl.web.rest;
 
+import com.klai.stl.domain.BillingAddress;
 import com.klai.stl.repository.BillingAddressRepository;
 import com.klai.stl.service.BillingAddressService;
 import com.klai.stl.service.dto.BillingAddressDTO;
@@ -8,23 +9,19 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.StreamSupport;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.jhipster.web.util.HeaderUtil;
-import tech.jhipster.web.util.ResponseUtil;
 
 /**
  * REST controller for managing {@link com.klai.stl.domain.BillingAddress}.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/company/{reference}")
 public class BillingAddressResource {
 
     private final Logger log = LoggerFactory.getLogger(BillingAddressResource.class);
@@ -44,15 +41,18 @@ public class BillingAddressResource {
     }
 
     /**
-     * {@code POST  /billing-addresses} : Create a new billingAddress.
+     * {@code POST  /billing} : Create a new billing.
      *
      * @param billingAddressDTO the billingAddressDTO to create.
+     * @param reference         the reference of a company
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new billingAddressDTO, or with status {@code 400 (Bad Request)} if the billingAddress has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PostMapping("/billing-addresses")
-    public ResponseEntity<BillingAddressDTO> createBillingAddress(@Valid @RequestBody BillingAddressDTO billingAddressDTO)
-        throws URISyntaxException {
+    @PostMapping("/billing")
+    public ResponseEntity<BillingAddressDTO> createBillingAddress(
+        @Valid @RequestBody BillingAddressDTO billingAddressDTO,
+        @PathVariable String reference
+    ) throws URISyntaxException {
         log.debug("REST request to save BillingAddress : {}", billingAddressDTO);
         if (billingAddressDTO.getId() != null) {
             throw new BadRequestAlertException("A new billingAddress cannot already have an ID", ENTITY_NAME, "idexists");
@@ -65,19 +65,20 @@ public class BillingAddressResource {
     }
 
     /**
-     * {@code PUT  /billing-addresses/:id} : Updates an existing billingAddress.
+     * {@code PUT  /billing} : Updates an existing billingAddress.
      *
-     * @param id the id of the billingAddressDTO to save.
      * @param billingAddressDTO the billingAddressDTO to update.
+     * @param reference         the reference of a company
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated billingAddressDTO,
      * or with status {@code 400 (Bad Request)} if the billingAddressDTO is not valid,
      * or with status {@code 500 (Internal Server Error)} if the billingAddressDTO couldn't be updated.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PutMapping("/billing-addresses/{id}")
+    @PutMapping("/billing-addresses/")
     public ResponseEntity<BillingAddressDTO> updateBillingAddress(
-        @PathVariable(value = "id", required = false) final Long id,
-        @Valid @RequestBody BillingAddressDTO billingAddressDTO
+        final Long id,
+        @Valid @RequestBody BillingAddressDTO billingAddressDTO,
+        @PathVariable String reference
     ) throws URISyntaxException {
         log.debug("REST request to update BillingAddress : {}, {}", id, billingAddressDTO);
         if (billingAddressDTO.getId() == null) {
@@ -99,83 +100,14 @@ public class BillingAddressResource {
     }
 
     /**
-     * {@code PATCH  /billing-addresses/:id} : Partial updates given fields of an existing billingAddress, field will ignore if it is null
+     * {@code GET  /billing} : get the billing address
      *
-     * @param id the id of the billingAddressDTO to save.
-     * @param billingAddressDTO the billingAddressDTO to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated billingAddressDTO,
-     * or with status {@code 400 (Bad Request)} if the billingAddressDTO is not valid,
-     * or with status {@code 404 (Not Found)} if the billingAddressDTO is not found,
-     * or with status {@code 500 (Internal Server Error)} if the billingAddressDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PatchMapping(value = "/billing-addresses/{id}", consumes = "application/merge-patch+json")
-    public ResponseEntity<BillingAddressDTO> partialUpdateBillingAddress(
-        @PathVariable(value = "id", required = false) final Long id,
-        @NotNull @RequestBody BillingAddressDTO billingAddressDTO
-    ) throws URISyntaxException {
-        log.debug("REST request to partial update BillingAddress partially : {}, {}", id, billingAddressDTO);
-        if (billingAddressDTO.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        if (!Objects.equals(id, billingAddressDTO.getId())) {
-            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
-        }
-
-        if (!billingAddressRepository.existsById(id)) {
-            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
-        }
-
-        Optional<BillingAddressDTO> result = billingAddressService.partialUpdate(billingAddressDTO);
-
-        return ResponseUtil.wrapOrNotFound(
-            result,
-            HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, billingAddressDTO.getId().toString())
-        );
-    }
-
-    /**
-     * {@code GET  /billing-addresses} : get all the billingAddresses.
-     *
-     * @param filter the filter of the request.
+     * @param reference the company reference
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of billingAddresses in body.
      */
-    @GetMapping("/billing-addresses")
-    public List<BillingAddressDTO> getAllBillingAddresses(@RequestParam(required = false) String filter) {
-        if ("company-is-null".equals(filter)) {
-            log.debug("REST request to get all BillingAddresss where company is null");
-            return billingAddressService.findAllWhereCompanyIsNull();
-        }
+    @GetMapping("/billing")
+    public List<BillingAddress> getBillingAddress(@PathVariable String reference) {
         log.debug("REST request to get all BillingAddresses");
-        return billingAddressService.findAll();
-    }
-
-    /**
-     * {@code GET  /billing-addresses/:id} : get the "id" billingAddress.
-     *
-     * @param id the id of the billingAddressDTO to retrieve.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the billingAddressDTO, or with status {@code 404 (Not Found)}.
-     */
-    @GetMapping("/billing-addresses/{id}")
-    public ResponseEntity<BillingAddressDTO> getBillingAddress(@PathVariable Long id) {
-        log.debug("REST request to get BillingAddress : {}", id);
-        Optional<BillingAddressDTO> billingAddressDTO = billingAddressService.findOne(id);
-        return ResponseUtil.wrapOrNotFound(billingAddressDTO);
-    }
-
-    /**
-     * {@code DELETE  /billing-addresses/:id} : delete the "id" billingAddress.
-     *
-     * @param id the id of the billingAddressDTO to delete.
-     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
-     */
-    @DeleteMapping("/billing-addresses/{id}")
-    public ResponseEntity<Void> deleteBillingAddress(@PathVariable Long id) {
-        log.debug("REST request to delete BillingAddress : {}", id);
-        billingAddressService.delete(id);
-        return ResponseEntity
-            .noContent()
-            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
-            .build();
+        return billingAddressRepository.findAll();
     }
 }
