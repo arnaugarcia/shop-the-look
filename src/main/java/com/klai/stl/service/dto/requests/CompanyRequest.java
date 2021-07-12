da7c6e87d1317cad1092c5572291f5c2afb17fb6
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-07-12 18:08:17 +0200

    Created CompanyRequest base object #31

---
+++ src/main/java/com/klai/stl/service/dto/requests/CompanyRequest.java
@@ -0,0 +1,43 @@
+package com.klai.stl.service.dto.requests;
+
+import com.klai.stl.domain.enumeration.CompanyIndustry;
+import com.klai.stl.domain.enumeration.CompanySize;
+import com.klai.stl.domain.enumeration.CompanyType;
+import com.klai.stl.service.dto.SubscriptionPlanDTO;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public abstract class CompanyRequest {
+
+    @NotNull
+    private final String name;
+
+    private final String commercialName;
+
+    @NotNull
+    private final String nif;
+
+    private final String logo;
+
+    private final String vat;
+
+    @NotNull
+    private final String url;
+
+    @NotNull
+    private final String phone;
+
+    @NotNull
+    private final String email;
+
+    private final CompanyType type;
+
+    private final CompanyIndustry industry;
+
+    private final CompanySize companySize;
+
+    private final SubscriptionPlanDTO subscriptionPlan;
+}
