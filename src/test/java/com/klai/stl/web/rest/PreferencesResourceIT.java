a0da5ed5cb22958c7cc6e1a697cc6def08e9746c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-07-21 19:37:05 +0200

    Created PreferencesResourceIT #39

--- src/test/java/com/klai/stl/web/rest/PreferencesResourceIT.java
+++ src/test/java/com/klai/stl/web/rest/PreferencesResourceIT.java
@@ -1,28 +1,15 @@
 package com.klai.stl.web.rest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasItem;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.klai.stl.IntegrationTest;
 import com.klai.stl.domain.Preferences;
 import com.klai.stl.domain.enumeration.ImportMethod;
 import com.klai.stl.repository.PreferencesRepository;
-import com.klai.stl.service.dto.PreferencesDTO;
-import com.klai.stl.service.mapper.PreferencesMapper;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Integration tests for the {@link Preferences}.
@@ -41,14 +28,8 @@ class PreferencesResourceIT {
     private static final String ENTITY_API_URL = "/api/preferences";
     private static final String ENTITY_API_URL_ID = ENTITY_API_URL + "/{id}";
 
-    private static Random random = new Random();
-    private static AtomicLong count = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
-
     @Autowired
     private PreferencesRepository preferencesRepository;
-
-    @Autowired
-    private PreferencesMapper preferencesMapper;
 
     @Autowired
     private EntityManager em;
@@ -83,249 +64,5 @@ class PreferencesResourceIT {
     @BeforeEach
     public void initTest() {
         preferences = createEntity(em);
-    }
-
-    @Test
-    @Transactional
-    void createPreferences() throws Exception {
-        int databaseSizeBeforeCreate = preferencesRepository.findAll().size();
-        // Create the Preferences
-        PreferencesDTO preferencesDTO = preferencesMapper.toDto(preferences);
-        restPreferencesMockMvc
-            .perform(
-                post(ENTITY_API_URL).contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(preferencesDTO))
-            )
-            .andExpect(status().isCreated());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeCreate + 1);
-        Preferences testPreferences = preferencesList.get(preferencesList.size() - 1);
-        assertThat(testPreferences.getImportMethod()).isEqualTo(DEFAULT_IMPORT_METHOD);
-        assertThat(testPreferences.getFeedUrl()).isEqualTo(DEFAULT_FEED_URL);
-    }
-
-    @Test
-    @Transactional
-    void createPreferencesWithExistingId() throws Exception {
-        // Create the Preferences with an existing ID
-        preferences.setId(1L);
-        PreferencesDTO preferencesDTO = preferencesMapper.toDto(preferences);
-
-        int databaseSizeBeforeCreate = preferencesRepository.findAll().size();
-
-        // An entity with an existing ID cannot be created, so this API call must fail
-        restPreferencesMockMvc
-            .perform(
-                post(ENTITY_API_URL).contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(preferencesDTO))
-            )
-            .andExpect(status().isBadRequest());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeCreate);
-    }
-
-    @Test
-    @Transactional
-    void getAllPreferences() throws Exception {
-        // Initialize the database
-        preferencesRepository.saveAndFlush(preferences);
-
-        // Get all the preferencesList
-        restPreferencesMockMvc
-            .perform(get(ENTITY_API_URL + "?sort=id,desc"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(jsonPath("$.[*].id").value(hasItem(preferences.getId().intValue())))
-            .andExpect(jsonPath("$.[*].importMethod").value(hasItem(DEFAULT_IMPORT_METHOD.toString())))
-            .andExpect(jsonPath("$.[*].feedUrl").value(hasItem(DEFAULT_FEED_URL)));
-    }
-
-    @Test
-    @Transactional
-    void getPreferences() throws Exception {
-        // Initialize the database
-        preferencesRepository.saveAndFlush(preferences);
-
-        // Get the preferences
-        restPreferencesMockMvc
-            .perform(get(ENTITY_API_URL_ID, preferences.getId()))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(jsonPath("$.id").value(preferences.getId().intValue()))
-            .andExpect(jsonPath("$.importMethod").value(DEFAULT_IMPORT_METHOD.toString()))
-            .andExpect(jsonPath("$.feedUrl").value(DEFAULT_FEED_URL));
-    }
-
-    @Test
-    @Transactional
-    void getNonExistingPreferences() throws Exception {
-        // Get the preferences
-        restPreferencesMockMvc.perform(get(ENTITY_API_URL_ID, Long.MAX_VALUE)).andExpect(status().isNotFound());
-    }
-
-    @Test
-    @Transactional
-    void putWithIdMismatchPreferences() throws Exception {
-        int databaseSizeBeforeUpdate = preferencesRepository.findAll().size();
-        preferences.setId(count.incrementAndGet());
-
-        // Create the Preferences
-        PreferencesDTO preferencesDTO = preferencesMapper.toDto(preferences);
-
-        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
-        restPreferencesMockMvc
-            .perform(
-                put(ENTITY_API_URL_ID, count.incrementAndGet())
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(TestUtil.convertObjectToJsonBytes(preferencesDTO))
-            )
-            .andExpect(status().isBadRequest());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeUpdate);
-    }
-
-    @Test
-    @Transactional
-    void putWithMissingIdPathParamPreferences() throws Exception {
-        int databaseSizeBeforeUpdate = preferencesRepository.findAll().size();
-        preferences.setId(count.incrementAndGet());
-
-        // Create the Preferences
-        PreferencesDTO preferencesDTO = preferencesMapper.toDto(preferences);
-
-        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
-        restPreferencesMockMvc
-            .perform(put(ENTITY_API_URL).contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(preferencesDTO)))
-            .andExpect(status().isMethodNotAllowed());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeUpdate);
-    }
-
-    @Test
-    @Transactional
-    void partialUpdatePreferencesWithPatch() throws Exception {
-        // Initialize the database
-        preferencesRepository.saveAndFlush(preferences);
-
-        int databaseSizeBeforeUpdate = preferencesRepository.findAll().size();
-
-        // Update the preferences using partial update
-        Preferences partialUpdatedPreferences = new Preferences();
-        partialUpdatedPreferences.setId(preferences.getId());
-
-        partialUpdatedPreferences.feedUrl(UPDATED_FEED_URL);
-
-        restPreferencesMockMvc
-            .perform(
-                patch(ENTITY_API_URL_ID, partialUpdatedPreferences.getId())
-                    .contentType("application/merge-patch+json")
-                    .content(TestUtil.convertObjectToJsonBytes(partialUpdatedPreferences))
-            )
-            .andExpect(status().isOk());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeUpdate);
-        Preferences testPreferences = preferencesList.get(preferencesList.size() - 1);
-        assertThat(testPreferences.getImportMethod()).isEqualTo(DEFAULT_IMPORT_METHOD);
-        assertThat(testPreferences.getFeedUrl()).isEqualTo(UPDATED_FEED_URL);
-    }
-
-    @Test
-    @Transactional
-    void fullUpdatePreferencesWithPatch() throws Exception {
-        // Initialize the database
-        preferencesRepository.saveAndFlush(preferences);
-
-        int databaseSizeBeforeUpdate = preferencesRepository.findAll().size();
-
-        // Update the preferences using partial update
-        Preferences partialUpdatedPreferences = new Preferences();
-        partialUpdatedPreferences.setId(preferences.getId());
-
-        partialUpdatedPreferences.importMethod(UPDATED_IMPORT_METHOD).feedUrl(UPDATED_FEED_URL);
-
-        restPreferencesMockMvc
-            .perform(
-                patch(ENTITY_API_URL_ID, partialUpdatedPreferences.getId())
-                    .contentType("application/merge-patch+json")
-                    .content(TestUtil.convertObjectToJsonBytes(partialUpdatedPreferences))
-            )
-            .andExpect(status().isOk());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeUpdate);
-        Preferences testPreferences = preferencesList.get(preferencesList.size() - 1);
-        assertThat(testPreferences.getImportMethod()).isEqualTo(UPDATED_IMPORT_METHOD);
-        assertThat(testPreferences.getFeedUrl()).isEqualTo(UPDATED_FEED_URL);
-    }
-
-    @Test
-    @Transactional
-    void patchWithIdMismatchPreferences() throws Exception {
-        int databaseSizeBeforeUpdate = preferencesRepository.findAll().size();
-        preferences.setId(count.incrementAndGet());
-
-        // Create the Preferences
-        PreferencesDTO preferencesDTO = preferencesMapper.toDto(preferences);
-
-        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
-        restPreferencesMockMvc
-            .perform(
-                patch(ENTITY_API_URL_ID, count.incrementAndGet())
-                    .contentType("application/merge-patch+json")
-                    .content(TestUtil.convertObjectToJsonBytes(preferencesDTO))
-            )
-            .andExpect(status().isBadRequest());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeUpdate);
-    }
-
-    @Test
-    @Transactional
-    void patchWithMissingIdPathParamPreferences() throws Exception {
-        int databaseSizeBeforeUpdate = preferencesRepository.findAll().size();
-        preferences.setId(count.incrementAndGet());
-
-        // Create the Preferences
-        PreferencesDTO preferencesDTO = preferencesMapper.toDto(preferences);
-
-        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
-        restPreferencesMockMvc
-            .perform(
-                patch(ENTITY_API_URL).contentType("application/merge-patch+json").content(TestUtil.convertObjectToJsonBytes(preferencesDTO))
-            )
-            .andExpect(status().isMethodNotAllowed());
-
-        // Validate the Preferences in the database
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeUpdate);
-    }
-
-    @Test
-    @Transactional
-    void deletePreferences() throws Exception {
-        // Initialize the database
-        preferencesRepository.saveAndFlush(preferences);
-
-        int databaseSizeBeforeDelete = preferencesRepository.findAll().size();
-
-        // Delete the preferences
-        restPreferencesMockMvc
-            .perform(delete(ENTITY_API_URL_ID, preferences.getId()).accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isNoContent());
-
-        // Validate the database contains one less item
-        List<Preferences> preferencesList = preferencesRepository.findAll();
-        assertThat(preferencesList).hasSize(databaseSizeBeforeDelete - 1);
     }
 }
