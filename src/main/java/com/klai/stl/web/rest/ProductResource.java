10e1e0f0531e90f869c13c7782b9b73f5c56835b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-07-23 19:01:08 +0200

    Removed PATCH and count endpoints of product resource #38

--- src/main/java/com/klai/stl/web/rest/ProductResource.java
+++ src/main/java/com/klai/stl/web/rest/ProductResource.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -102,42 +101,6 @@ public class ProductResource {
     }
 
     /**
-     * {@code PATCH  /products/:id} : Partial updates given fields of an existing product, field will ignore if it is null
-     *
-     * @param id the id of the productDTO to save.
-     * @param productDTO the productDTO to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated productDTO,
-     * or with status {@code 400 (Bad Request)} if the productDTO is not valid,
-     * or with status {@code 404 (Not Found)} if the productDTO is not found,
-     * or with status {@code 500 (Internal Server Error)} if the productDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PatchMapping(value = "/products/{id}", consumes = "application/merge-patch+json")
-    public ResponseEntity<ProductDTO> partialUpdateProduct(
-        @PathVariable(value = "id", required = false) final Long id,
-        @NotNull @RequestBody ProductDTO productDTO
-    ) throws URISyntaxException {
-        log.debug("REST request to partial update Product partially : {}, {}", id, productDTO);
-        if (productDTO.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        if (!Objects.equals(id, productDTO.getId())) {
-            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
-        }
-
-        if (!productRepository.existsById(id)) {
-            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
-        }
-
-        Optional<ProductDTO> result = productService.partialUpdate(productDTO);
-
-        return ResponseUtil.wrapOrNotFound(
-            result,
-            HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, productDTO.getId().toString())
-        );
-    }
-
-    /**
      * {@code GET  /products} : get all the products.
      *
      * @param criteria the criteria which the requested entities should match.
@@ -148,18 +111,6 @@ public class ProductResource {
         log.debug("REST request to get Products by criteria: {}", criteria);
         List<ProductDTO> entityList = productQueryService.findByCriteria(criteria);
         return ResponseEntity.ok().body(entityList);
-    }
-
-    /**
-     * {@code GET  /products/count} : count all the products.
-     *
-     * @param criteria the criteria which the requested entities should match.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the count in body.
-     */
-    @GetMapping("/products/count")
-    public ResponseEntity<Long> countProducts(ProductCriteria criteria) {
-        log.debug("REST request to count Products by criteria: {}", criteria);
-        return ResponseEntity.ok().body(productQueryService.countByCriteria(criteria));
     }
 
     /**
