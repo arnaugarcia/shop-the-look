9e50d89b46790a7405d18df82f07bf1d3ea6237b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-07-23 17:38:14 +0200

    Set PreferencesNotFound as NotFound error #39

--- src/main/java/com/klai/stl/web/rest/errors/PreferencesNotFound.java
+++ src/main/java/com/klai/stl/web/rest/errors/PreferencesNotFound.java
@@ -2,7 +2,7 @@ package com.klai.stl.web.rest.errors;
 
 import static com.klai.stl.web.rest.errors.ErrorConstants.INTERNAL_SERVER;
 
-public class PreferencesNotFound extends InternalServerErrorException {
+public class PreferencesNotFound extends NotFoundException {
 
     private static final long serialVersionUID = 1L;
 
