3e38decefd9e59f80c4a502b2881313724546f2c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-07-23 17:59:16 +0200

    Added URL annotation validation for PreferencesRequest #39

--- src/main/java/com/klai/stl/service/dto/requests/PreferencesRequest.java
+++ src/main/java/com/klai/stl/service/dto/requests/PreferencesRequest.java
@@ -1,9 +1,11 @@
 package com.klai.stl.service.dto.requests;
 
 import com.klai.stl.domain.enumeration.ImportMethod;
+import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
+import org.hibernate.validator.constraints.URL;
 
 @Value
 @Builder
@@ -12,5 +14,7 @@ public class PreferencesRequest {
 
     private final ImportMethod importMethod;
 
+    @URL
+    @NotBlank
     private final String feedUrl;
 }
