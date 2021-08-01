536871c5d9adf89e77fac3b6edea2ff697300346
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-08-01 16:06:10 +0200

    Finished integrating password-reset-init component #43

--- src/main/webapp/app/auth/password-reset/init/password-reset-init.component.ts
+++ src/main/webapp/app/auth/password-reset/init/password-reset-init.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 import { PasswordResetInitService } from './password-reset-init.service';
+import { CoreConfigService } from '../../../../@core/services/config.service';
 
 @Component({
   selector: 'stl-password-reset-init',
@@ -16,8 +17,29 @@ export class PasswordResetInitComponent implements AfterViewInit {
   resetRequestForm = this.fb.group({
     email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
   });
+  submitted = false;
 
-  constructor(private passwordResetInitService: PasswordResetInitService, private fb: FormBuilder) {}
+  constructor(
+    private coreConfigService: CoreConfigService,
+    private passwordResetInitService: PasswordResetInitService,
+    private fb: FormBuilder
+  ) {
+    this.coreConfigService.setConfig({
+      layout: {
+        navbar: {
+          hidden: true,
+        },
+        menu: {
+          hidden: true,
+        },
+        footer: {
+          hidden: true,
+        },
+        customizer: false,
+        enableLocalStorage: false,
+      },
+    });
+  }
 
   ngAfterViewInit(): void {
     if (this.email) {
@@ -26,6 +48,7 @@ export class PasswordResetInitComponent implements AfterViewInit {
   }
 
   requestReset(): void {
+    this.submitted = true;
     this.passwordResetInitService.save(this.resetRequestForm.get(['email'])!.value).subscribe(() => (this.success = true));
   }
 }
