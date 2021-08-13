f644de06295218e2e787ee7d9ac3b597d42ee88d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-08-13 13:37:55 +0200

    Fixed bad extension on user not found exception #39 #38

--- src/main/java/com/klai/stl/service/exception/UserNotFound.java
+++ src/main/java/com/klai/stl/service/exception/UserNotFound.java
@@ -1,12 +1,8 @@
 package com.klai.stl.service.exception;
 
-import static com.klai.stl.web.rest.errors.ErrorConstants.NOT_FOUND;
-
-import com.klai.stl.web.rest.errors.NotFoundException;
-
-public class UserNotFound extends NotFoundException {
+public class UserNotFound extends RuntimeException {
 
     public UserNotFound() {
-        super(NOT_FOUND, "No current user found", "user", "usernotfound");
+        super("User not found");
     }
 }
