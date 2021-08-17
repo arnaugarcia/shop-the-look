850c38afd7bfade2386928b4a1158198f7013dc3
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-08-17 14:20:41 +0200

    Added logic for adding employee to a company #59

--- src/main/webapp/app/features/employee/models/employee.model.ts
+++ src/main/webapp/app/features/employee/models/employee.model.ts
@@ -16,12 +16,25 @@ export interface IEmployee {
 
 export class Employee implements IEmployee {
   constructor(
+    public firstName: string,
+    public lastName: string,
+    public imageUrl: string,
+    public status: AccountStatus,
+    public login: string,
+    public email: string,
+    public authorities: string[]
+  ) {}
+}
+
+export class EmployeeRequest implements IEmployee {
+  constructor(
     public firstName?: string,
     public lastName?: string,
     public imageUrl?: string,
     public status?: AccountStatus,
     public login?: string,
     public email?: string,
-    public authorities?: string[]
+    public authorities?: string[],
+    public companyReference?: string
   ) {}
 }
