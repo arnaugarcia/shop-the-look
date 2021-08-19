4b8945abbdc782ed5b42a867111cf983d8fcb169
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-08-19 14:13:28 +0200

    Added logic for saving billing address #60

--- src/main/webapp/app/features/account/pages/billing/billing.component.ts
+++ src/main/webapp/app/features/account/pages/billing/billing.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { BillingAddress } from '../../model/billing-address.model';
+import { BillingAddress, IBillingAddress } from '../../model/billing-address.model';
 import { BillingAddressService } from '../../service/billing-address.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'stl-billing',
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.scss'],
 })
-export class BillingComponent {
+export class BillingComponent implements OnInit {
   public billingAddress = new BillingAddress();
 
   billingAddressForm = this.formBuilder.group({
@@ -18,10 +22,56 @@ export class BillingComponent {
     zipCode: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]],
     province: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
   });
+  private isSaving = false;
 
-  constructor(private formBuilder: FormBuilder, private billingAddressService: BillingAddressService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private billingAddressService: BillingAddressService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(({ billingAddress }) => {
+      this.updateForm(billingAddress);
+    });
+  }
 
   save(): void {
-    console.error('saved!');
+    this.isSaving = true;
+    const billingAddress = this.createFromForm();
+    this.subscribeToSaveResponse(this.billingAddressService.update(billingAddress));
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<IBillingAddress>>): void {
+    result.pipe(finalize(() => this.onSaveFinalize())).subscribe(null, () => this.onSaveError());
+  }
+
+  protected onSaveFinalize(): void {
+    this.isSaving = false;
+  }
+
+  protected onSaveError(): void {
+    // Api for inheritance.
+  }
+
+  protected createFromForm(): IBillingAddress {
+    return {
+      ...new BillingAddress(),
+      address: this.billingAddressForm.get(['address'])!.value,
+      city: this.billingAddressForm.get(['city'])!.value,
+      country: this.billingAddressForm.get(['country'])!.value,
+      zipCode: this.billingAddressForm.get(['zipCode'])!.value,
+      province: this.billingAddressForm.get(['province'])!.value,
+    };
+  }
+
+  private updateForm(billingAddress: IBillingAddress): void {
+    this.billingAddressForm.patchValue({
+      address: billingAddress.address,
+      city: billingAddress.city,
+      country: billingAddress.country,
+      zipCode: billingAddress.zipCode,
+      province: billingAddress.province,
+    });
   }
 }
