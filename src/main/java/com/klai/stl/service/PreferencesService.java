e5c008cb9c7ccff24dbf8818e800f0164538c0c2
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-08-22 14:13:22 +0200

    Added logic for getting company preferences by current user #8

--- src/main/java/com/klai/stl/service/PreferencesService.java
+++ src/main/java/com/klai/stl/service/PreferencesService.java
@@ -5,7 +5,23 @@ import com.klai.stl.service.dto.requests.PreferencesRequest;
 
 public interface PreferencesService {
     /**
+     * Find company preferences of the company reference by current User
+     *
+     * @return the preferences of the company
+     */
+    PreferencesDTO findByCurrentUser();
+
+    /**
+     * Updates the preferences of the desired company
+     *
+     * @param preferencesRequest the preferences request to update
+     * @return the updated preferences
+     */
+    PreferencesDTO updateByCurrentUser(PreferencesRequest preferencesRequest);
+
+    /**
      * Find company preferences by company reference
+     *
      * @param companyReference the company reference
      * @return the preferences of the company
      */
--- src/main/java/com/klai/stl/web/rest/CompanyPreferencesResource.java
+++ src/main/java/com/klai/stl/web/rest/CompanyPreferencesResource.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
  * REST controller for managing {@link com.klai.stl.domain.Preferences}.
  */
 @RestController
-@RequestMapping("/api/companies/{reference}")
+@RequestMapping("/api")
 public class CompanyPreferencesResource {
 
     private final Logger log = LoggerFactory.getLogger(CompanyPreferencesResource.class);
@@ -32,7 +32,7 @@ public class CompanyPreferencesResource {
      * @param reference the reference of the company preferences to update
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the preferencesDTO, or with status {@code 404 (Not Found)}.
      */
-    @PutMapping("/preferences")
+    @PutMapping("/companies/{reference}/preferences")
     public ResponseEntity<PreferencesDTO> updatePreferences(
         @PathVariable String reference,
         @Valid @RequestBody PreferencesRequest preferencesRequest
@@ -47,9 +47,31 @@ public class CompanyPreferencesResource {
      * @param reference the company reference
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the preferences in body.
      */
-    @GetMapping("/preferences")
+    @GetMapping("/companies/{reference}/preferences")
     public ResponseEntity<PreferencesDTO> getPreferences(@PathVariable String reference) {
-        log.debug("REST request to get all BillingAddresses");
+        log.debug("REST request to get company preferences");
         return ok(preferencesService.find(reference));
+    }
+
+    /**
+     * {@code GET  /preferences} : get the preferences of a company
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the preferences in body.
+     */
+    @GetMapping("/preferences")
+    public ResponseEntity<PreferencesDTO> getCurrentCompanyPreferences() {
+        log.debug("REST request to get company preferences");
+        return ok(preferencesService.findByCurrentUser());
+    }
+
+    /**
+     * {@code PUT  /preferences} : update the company preferences
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the preferencesDTO, or with status {@code 404 (Not Found)}.
+     */
+    @PutMapping("/preferences")
+    public ResponseEntity<PreferencesDTO> updateCurrentCompanyPreferences(@Valid @RequestBody PreferencesRequest preferencesRequest) {
+        log.debug("REST request to update a Company preference of the current user");
+        return ok(preferencesService.updateByCurrentUser(preferencesRequest));
     }
 }
