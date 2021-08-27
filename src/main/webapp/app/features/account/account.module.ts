e81272d95681476e3a9a44592645e05d59ad5ceb
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-08-27 13:35:44 +0200

    Removed preferences service and component #80

--- src/main/webapp/app/features/account/account.module.ts
+++ src/main/webapp/app/features/account/account.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { AccountRoutingModule } from './account-routing.module.route';
 import { SettingsComponent } from './pages/settings/settings.component';
-import { PreferencesComponent } from './pages/preferences/preferences.component';
 import { AccountComponent } from './pages/account/account.component';
 import { ContentHeaderModule } from '../../layouts/content-header/content-header.module';
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 import { PasswordComponent } from './pages/password/password.component';
 
 @NgModule({
-  declarations: [SettingsComponent, PreferencesComponent, AccountComponent, NotificationsComponent, PasswordComponent],
+  declarations: [SettingsComponent, AccountComponent, NotificationsComponent, PasswordComponent],
   imports: [SharedModule, ContentHeaderModule, AccountRoutingModule],
 })
 export class AccountModule {}
--- src/main/webapp/app/features/account/pages/preferences/preferences.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'stl-preferences',
-  templateUrl: './preferences.component.html',
-  styleUrls: ['./preferences.component.scss'],
-})
-export class PreferencesComponent {}
