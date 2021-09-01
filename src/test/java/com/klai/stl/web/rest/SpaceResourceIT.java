c3f43f697420f204318e19f4cb9f5a5915020156
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-01 14:00:21 +0200

    Added companyReference param to admin space resource #42

--- src/test/java/com/klai/stl/web/rest/SpaceResourceIT.java
+++ src/test/java/com/klai/stl/web/rest/SpaceResourceIT.java
@@ -1,8 +1,22 @@
 package com.klai.stl.web.rest;
 
+import static com.klai.stl.security.AuthoritiesConstants.ADMIN;
+import static com.klai.stl.security.AuthoritiesConstants.MANAGER;
+import static com.klai.stl.service.dto.requests.space.SpaceRequest.builder;
+import static com.klai.stl.web.rest.CompanyResourceIT.createBasicCompany;
+import static com.klai.stl.web.rest.TestUtil.convertObjectToJsonBytes;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.klai.stl.IntegrationTest;
+import com.klai.stl.domain.Company;
+import com.klai.stl.domain.Space;
+import com.klai.stl.domain.User;
 import com.klai.stl.repository.SpaceRepository;
 import com.klai.stl.service.dto.requests.space.SpaceRequest;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,8 +45,9 @@ class SpaceResourceIT {
     private static final String DEFAULT_DESCRIPTION = "AAAAAAAAAA";
     private static final String UPDATED_DESCRIPTION = "BBBBBBBBBB";
 
-    private static final String ENTITY_API_URL_ADMIN = "/api/spaces";
-    private static final String ENTITY_API_URL = "/api/spaces";
+    private static final String API_URL_ADMIN = "/api/spaces?companyReference={reference}";
+    private static final String API_URL = "/api/spaces";
+    private static final String ME_API_URL = "/api/me/spaces";
 
     @Autowired
     private SpaceRepository spaceRepository;
@@ -43,39 +58,126 @@ class SpaceResourceIT {
     @Autowired
     private MockMvc restSpaceMockMvc;
 
+    private Company company;
+
     private SpaceRequest spaceRequest;
 
     @BeforeEach
     public void initTest() {
         this.spaceRequest = buildRequest();
+        this.company = createBasicCompany(em);
     }
 
     private SpaceRequest buildRequest() {
-        return SpaceRequest.builder().name(DEFAULT_NAME).description(DEFAULT_DESCRIPTION).build();
+        return builder().name(DEFAULT_NAME).description(DEFAULT_DESCRIPTION).build();
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username = "space-create-user")
+    public void createSpaceAsUser() throws Exception {
+        final User user = UserResourceIT.createEntity(em, "space-create-user");
+        company.addUser(user);
+        em.persist(company);
+
+        int databaseSizeBeforeCreate = spaceRepository.findByCompanyReference(company.getReference()).size();
+
+        restSpaceMockMvc
+            .perform(post(ME_API_URL).contentType(APPLICATION_JSON).content(convertObjectToJsonBytes(spaceRequest)))
+            .andExpect(status().isCreated());
+
+        int databaseSizeAfterCreate = spaceRepository.findByCompanyReference(company.getReference()).size();
+        assertThat(databaseSizeBeforeCreate).isGreaterThan(databaseSizeAfterCreate);
+
+        final Optional<Space> spaceOptional = spaceRepository.findByCompanyReference(company.getReference()).stream().findFirst();
+        assertThat(spaceOptional).isPresent();
+
+        Space result = spaceOptional.get();
+        assertThat(result.getReference()).isNotBlank();
+        assertThat(result.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(result.getDescription()).isEqualTo(DEFAULT_DESCRIPTION);
+        assertThat(result.getActive()).isFalse();
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(authorities = MANAGER, username = "space-create-manager")
+    public void createSpaceAsManager() throws Exception {
+        final User user = UserResourceIT.createEntity(em, "space-create-manager");
+        company.addUser(user);
+        em.persist(company);
+
+        int databaseSizeBeforeCreate = spaceRepository.findByCompanyReference(company.getReference()).size();
+
+        restSpaceMockMvc
+            .perform(post(ME_API_URL).contentType(APPLICATION_JSON).content(convertObjectToJsonBytes(spaceRequest)))
+            .andExpect(status().isCreated());
+
+        int databaseSizeAfterCreate = spaceRepository.findByCompanyReference(company.getReference()).size();
+        assertThat(databaseSizeBeforeCreate).isGreaterThan(databaseSizeAfterCreate);
+
+        final Optional<Space> spaceOptional = spaceRepository.findByCompanyReference(company.getReference()).stream().findFirst();
+        assertThat(spaceOptional).isPresent();
+
+        Space result = spaceOptional.get();
+        assertThat(result.getReference()).isNotBlank();
+        assertThat(result.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(result.getDescription()).isEqualTo(DEFAULT_DESCRIPTION);
+        assertThat(result.getActive()).isFalse();
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(authorities = ADMIN)
+    public void createSpaceForOtherCompanyAsAdmin() throws Exception {
+        int databaseSizeBeforeCreate = spaceRepository.findByCompanyReference(company.getReference()).size();
+
+        restSpaceMockMvc
+            .perform(
+                post(API_URL_ADMIN, company.getReference()).contentType(APPLICATION_JSON).content(convertObjectToJsonBytes(spaceRequest))
+            )
+            .andExpect(status().isCreated());
+
+        int databaseSizeAfterCreate = spaceRepository.findByCompanyReference(company.getReference()).size();
+        assertThat(databaseSizeBeforeCreate).isGreaterThan(databaseSizeAfterCreate);
+
+        final Optional<Space> spaceOptional = spaceRepository.findByCompanyReference(company.getReference()).stream().findFirst();
+        assertThat(spaceOptional).isPresent();
+
+        Space result = spaceOptional.get();
+        assertThat(result.getReference()).isNotBlank();
+        assertThat(result.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(result.getDescription()).isEqualTo(DEFAULT_DESCRIPTION);
+        assertThat(result.getActive()).isFalse();
     }
 
     @Test
     @Transactional
     @WithMockUser
-    public void createSpaceAsUser() {}
+    public void createSpaceForOtherCompanyAsManager() throws Exception {
+        final User user = UserResourceIT.createEntity(em, "space-create-admin");
+        company.addUser(user);
+        em.persist(company);
+
+        restSpaceMockMvc
+            .perform(
+                post(API_URL_ADMIN, company.getReference()).contentType(APPLICATION_JSON).content(convertObjectToJsonBytes(spaceRequest))
+            )
+            .andExpect(status().isForbidden());
+    }
 
     @Test
     @Transactional
     @WithMockUser
-    public void createSpaceAsManager() {}
+    public void createSpaceForOtherCompanyAsUser() throws Exception {
+        final User user = UserResourceIT.createEntity(em, "space-create-admin");
+        company.addUser(user);
+        em.persist(company);
 
-    @Test
-    @Transactional
-    @WithMockUser
-    public void createSpaceAsAdmin() {}
-
-    @Test
-    @Transactional
-    @WithMockUser
-    public void createSpaceForOtherCompanyAsAdmin() {}
-
-    @Test
-    @Transactional
-    @WithMockUser
-    public void createSpaceForOtherCompanyAsManager() {}
+        restSpaceMockMvc
+            .perform(
+                post(API_URL_ADMIN, company.getReference()).contentType(APPLICATION_JSON).content(convertObjectToJsonBytes(spaceRequest))
+            )
+            .andExpect(status().isForbidden());
+    }
 }
