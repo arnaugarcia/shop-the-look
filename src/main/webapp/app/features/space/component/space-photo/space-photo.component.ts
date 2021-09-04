5848a882a31017d446ec39adb0bd663cf7bb6269
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-04 17:40:02 +0200

    Added template for displaying loaded photo #42

--- src/main/webapp/app/features/space/component/space-photo/space-photo.component.ts
+++ src/main/webapp/app/features/space/component/space-photo/space-photo.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
+import { SpaceService } from '../../service/space.service';
 
 @Component({
   selector: 'stl-space-photo',
@@ -7,13 +8,27 @@ import { FileUploader } from 'ng2-file-upload';
   styleUrls: ['./space-photo.component.scss'],
 })
 export class SpacePhotoComponent {
+  @Input()
+  public height: string;
+
+  @Input()
+  public width: string;
+
   public hasBaseDropZoneOver = false;
   public loading = false;
   public error = false;
 
+  public photoUrl =
+    'https://images.unsplash.com/photo-1630688231126-dd36840fce51?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80';
+
   public uploader: FileUploader = new FileUploader({
     isHTML5: true,
   });
+
+  constructor(private spaceService: SpaceService) {
+    this.height = 'auto';
+    this.width = 'auto';
+  }
 
   fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
