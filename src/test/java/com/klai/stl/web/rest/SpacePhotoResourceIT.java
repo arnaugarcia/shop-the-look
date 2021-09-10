12ae35e0818db126833513f431beff673a3b0ef5
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-10 20:00:11 +0200

    Trying to test private method #42

--- src/test/java/com/klai/stl/web/rest/SpacePhotoResourceIT.java
+++ src/test/java/com/klai/stl/web/rest/SpacePhotoResourceIT.java
@@ -3,8 +3,10 @@ package com.klai.stl.web.rest;
 import static com.klai.stl.web.rest.CompanyResourceIT.createBasicCompany;
 import static com.klai.stl.web.rest.SpaceResourceIT.createSpace;
 import static com.klai.stl.web.rest.TestUtil.convertObjectToJsonBytes;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.klai.stl.IntegrationTest;
@@ -12,12 +14,19 @@ import com.klai.stl.domain.Company;
 import com.klai.stl.domain.Space;
 import com.klai.stl.domain.User;
 import com.klai.stl.repository.SpaceRepository;
+import com.klai.stl.service.PhotoService;
+import com.klai.stl.service.UploadService;
 import com.klai.stl.service.dto.requests.space.SpacePhotoRequest;
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,6 +45,8 @@ class SpacePhotoResourceIT {
 
     private static final byte[] DEFAULT_DATA = new byte[100];
 
+    private static final String DEFAULT_IMAGE_URL = "https://arnaugarcia.com/giveyouup.mp3";
+
     private static final String API_URL_REFERENCE = "/api/spaces/{reference}/photos";
 
     @Autowired
@@ -47,6 +58,12 @@ class SpacePhotoResourceIT {
     @Autowired
     private MockMvc restSpaceMockMvc;
 
+    @MockBean
+    private UploadService uploadService;
+
+    @MockBean
+    private PhotoService photoService;
+
     private Company company;
 
     private Space space;
@@ -54,10 +71,13 @@ class SpacePhotoResourceIT {
     private SpacePhotoRequest spacePhotoRequest;
 
     @BeforeEach
-    public void initTest() {
+    public void initTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         company = createBasicCompany(em);
         space = createSpace(em, company);
         spacePhotoRequest = createRequest();
+        Method method = PhotoService.class.getDeclaredMethod("getImageDimension", Dimension.class);
+        method.setAccessible(true);
+        method.invoke(photoService, new Dimension(800, 600));
     }
 
     private SpacePhotoRequest createRequest() {
@@ -68,11 +88,12 @@ class SpacePhotoResourceIT {
     @Transactional
     @WithMockUser(username = "add-photo-user")
     public void addPhotoToSpace() throws Exception {
-        createAndAppendUserToCompanyByLogin("add-photo-user");
+        when(uploadService.uploadImage(any())).thenReturn(new URL(DEFAULT_IMAGE_URL));
 
+        createAndAppendUserToCompanyByLogin("add-photo-user");
         restSpaceMockMvc
             .perform(
-                put(API_URL_REFERENCE, space.getReference())
+                post(API_URL_REFERENCE, space.getReference())
                     .contentType(APPLICATION_JSON)
                     .content(convertObjectToJsonBytes(spacePhotoRequest))
             )
@@ -82,7 +103,7 @@ class SpacePhotoResourceIT {
     @Test
     @Transactional
     @WithMockUser
-    public void addPhotoToSpaceThatNotBelongsToCurretntUser() throws Exception {}
+    public void addPhotoToSpaceThatNotBelongsToCurrentUser() throws Exception {}
 
     @Test
     @Transactional
