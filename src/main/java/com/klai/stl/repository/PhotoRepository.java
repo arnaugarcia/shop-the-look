888856dcf6b301733ca1764572bf4a46a9c8c53e
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-12 14:22:13 +0200

    Created method for deleting photo by reference #42

--- src/main/java/com/klai/stl/repository/PhotoRepository.java
+++ src/main/java/com/klai/stl/repository/PhotoRepository.java
@@ -1,6 +1,7 @@
 package com.klai.stl.repository;
 
 import com.klai.stl.domain.Photo;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long>, JpaSpecificationExecutor<Photo> {
     void deleteByReference(String reference);
+
+    Optional<Photo> findByReference(String reference);
 }
