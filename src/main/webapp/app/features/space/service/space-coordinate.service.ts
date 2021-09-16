368f4e21b3945aa568040e88e0f5538edd61606b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-16 11:43:51 +0200

    Created space coordinate service #42

---
+++ src/main/webapp/app/features/space/service/space-coordinate.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApplicationConfigService } from '../../../core/config/application-config.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SpaceCoordinateService {
+  constructor(protected http: HttpClient, protected applicationConfigService: ApplicationConfigService) {}
+
+  addCoordinate(param: { productReference: string; x: any; photoReference: string; y: any }): void {
+    console.error(param);
+  }
+}
