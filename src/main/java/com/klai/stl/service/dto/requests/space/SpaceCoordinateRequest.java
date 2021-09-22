a31425c19504c93bf767a3b487355bd8c22ee1ed
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-22 18:41:43 +0200

    Added builder to SpaceCoordinateRequest #42

--- src/main/java/com/klai/stl/service/dto/requests/space/SpaceCoordinateRequest.java
+++ src/main/java/com/klai/stl/service/dto/requests/space/SpaceCoordinateRequest.java
@@ -1,9 +1,13 @@
 package com.klai.stl.service.dto.requests.space;
 
 import javax.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 @Value
+@Builder
+@Jacksonized
 public class SpaceCoordinateRequest {
 
     @NotNull
