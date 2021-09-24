72a0450a59e69b5dd0270f8ddf195749f6838483
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-24 16:19:19 +0200

    Removed product reference and photo reference and replaced with ProductDTO #42

--- src/main/java/com/klai/stl/service/mapper/CoordinateMapper.java
+++ src/main/java/com/klai/stl/service/mapper/CoordinateMapper.java
@@ -3,14 +3,11 @@ package com.klai.stl.service.mapper;
 import com.klai.stl.domain.Coordinate;
 import com.klai.stl.service.dto.CoordinateDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 /**
  * Mapper for the entity {@link Coordinate} and its DTO {@link CoordinateDTO}.
  */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { ProductMapper.class })
 public interface CoordinateMapper extends EntityMapper<CoordinateDTO, Coordinate> {
-    @Mapping(source = "product.reference", target = "productReference")
-    @Mapping(source = "photo.reference", target = "photoReference")
     CoordinateDTO toDto(Coordinate coordinate);
 }
