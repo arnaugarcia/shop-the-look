65bb4248cd1e5a1f0c2e532428c24906d921d71c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-24 13:48:30 +0200

    Added missing mappers on transforming an entity to a DTO #42

--- src/main/java/com/klai/stl/service/mapper/CoordinateMapper.java
+++ src/main/java/com/klai/stl/service/mapper/CoordinateMapper.java
@@ -3,11 +3,14 @@ package com.klai.stl.service.mapper;
 import com.klai.stl.domain.Coordinate;
 import com.klai.stl.service.dto.CoordinateDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Mapper for the entity {@link Coordinate} and its DTO {@link CoordinateDTO}.
  */
 @Mapper(componentModel = "spring")
 public interface CoordinateMapper extends EntityMapper<CoordinateDTO, Coordinate> {
-    CoordinateDTO toDto(Coordinate s);
+    @Mapping(source = "product.reference", target = "productReference")
+    @Mapping(source = "photo.reference", target = "photoReference")
+    CoordinateDTO toDto(Coordinate coordinate);
 }
