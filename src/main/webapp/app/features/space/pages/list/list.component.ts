1f86567e50dc682b4342d9f72f113bebb206cba6
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-26 13:02:07 +0200

    Added basic style for displaying spaces #42

--- src/main/webapp/app/features/space/pages/list/list.component.ts
+++ src/main/webapp/app/features/space/pages/list/list.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { ContentHeader } from '../../../../layouts/content-header/content-header.component';
 
 @Component({
   selector: 'stl-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent {}
+export class ListComponent {
+  contentHeader: ContentHeader;
+
+  constructor() {
+    this.contentHeader = {
+      headerTitle: 'Spaces',
+      actionButton: true,
+      breadcrumb: {
+        type: '',
+        links: [
+          {
+            name: 'Home',
+            isLink: true,
+            link: '/',
+          },
+          {
+            name: 'Spaces',
+            isLink: false,
+          },
+        ],
+      },
+    };
+  }
+}
