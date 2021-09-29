6fcd206e5737c9f371acdb7cb799e1c67ab4c885
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-09-29 14:37:09 +0200

    Added attribute path when query a space #42

--- src/main/java/com/klai/stl/repository/SpaceRepository.java
+++ src/main/java/com/klai/stl/repository/SpaceRepository.java
@@ -3,6 +3,7 @@ package com.klai.stl.repository;
 import com.klai.stl.domain.Space;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -14,6 +15,10 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface SpaceRepository extends JpaRepository<Space, Long>, JpaSpecificationExecutor<Space> {
+    @Override
+    @EntityGraph(attributePaths = { "photos", "photos.coordinates" })
+    List<Space> findAll(Specification<Space> specification);
+
     List<Space> findByCompanyReference(String companyReference);
 
     @EntityGraph(attributePaths = { "photos", "photos.coordinates" })
