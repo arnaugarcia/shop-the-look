7cf82bc4f926ecf8c7af84b84ff25d0309da0fdc
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-08 13:24:09 +0200

    Created pricing plan component and stored the main logic there component  #114

---
+++ src/main/webapp/app/features/company/component/pricing-plan/pricing-plan.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stl-pricing-plan',
+  templateUrl: './pricing-plan.component.html',
+  styleUrls: ['./pricing-plan.component.scss'],
+})
+export class PricingPlanComponent {}
