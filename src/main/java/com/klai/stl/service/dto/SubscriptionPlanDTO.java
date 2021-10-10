e917e8de1273e5bcb1359c208879b98c51afc303
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-10 14:03:58 +0200

    Removed unnecessary fields or subscription plan DTO #114

--- src/main/java/com/klai/stl/service/dto/SubscriptionPlanDTO.java
+++ src/main/java/com/klai/stl/service/dto/SubscriptionPlanDTO.java
@@ -2,15 +2,14 @@ package com.klai.stl.service.dto;
 
 import com.klai.stl.domain.enumeration.SubscriptionCategory;
 import java.io.Serializable;
-import java.util.Objects;
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
 
 /**
  * A DTO for the {@link com.klai.stl.domain.SubscriptionPlan} entity.
  */
+@Data
 public class SubscriptionPlanDTO implements Serializable {
-
-    private Long id;
 
     @NotNull
     private String name;
@@ -28,95 +27,4 @@ public class SubscriptionPlanDTO implements Serializable {
 
     @NotNull
     private Integer maxRequests;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public SubscriptionCategory getCategory() {
-        return category;
-    }
-
-    public void setCategory(SubscriptionCategory category) {
-        this.category = category;
-    }
-
-    public Integer getMaxProducts() {
-        return maxProducts;
-    }
-
-    public void setMaxProducts(Integer maxProducts) {
-        this.maxProducts = maxProducts;
-    }
-
-    public Integer getMaxSpaces() {
-        return maxSpaces;
-    }
-
-    public void setMaxSpaces(Integer maxSpaces) {
-        this.maxSpaces = maxSpaces;
-    }
-
-    public Integer getMaxRequests() {
-        return maxRequests;
-    }
-
-    public void setMaxRequests(Integer maxRequests) {
-        this.maxRequests = maxRequests;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof SubscriptionPlanDTO)) {
-            return false;
-        }
-
-        SubscriptionPlanDTO subscriptionPlanDTO = (SubscriptionPlanDTO) o;
-        if (this.id == null) {
-            return false;
-        }
-        return Objects.equals(this.id, subscriptionPlanDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id);
-    }
-
-    // prettier-ignore
-    @Override
-    public String toString() {
-        return "SubscriptionPlanDTO{" +
-            "id=" + getId() +
-            ", name='" + getName() + "'" +
-            ", description='" + getDescription() + "'" +
-            ", category='" + getCategory() + "'" +
-            ", maxProducts=" + getMaxProducts() +
-            ", maxSpaces=" + getMaxSpaces() +
-            ", maxRequests=" + getMaxRequests() +
-            "}";
-    }
 }
