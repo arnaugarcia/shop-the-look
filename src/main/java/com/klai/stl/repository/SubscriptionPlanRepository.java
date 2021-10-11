47c23be25cd53c84fb0042c3bf0d2cab84a62458
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-11 13:54:20 +0200

    Removed java-doc and suppress warnings annotation #118

--- src/main/java/com/klai/stl/repository/SubscriptionPlanRepository.java
+++ src/main/java/com/klai/stl/repository/SubscriptionPlanRepository.java
@@ -9,10 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-/**
- * Spring Data SQL repository for the SubscriptionPlan entity.
- */
-@SuppressWarnings("unused")
 @Repository
 public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Long> {
     Optional<SubscriptionPlan> findByReference(String subscriptionReference);
