446a2cd6fc4b8b4520858129c15e1c84de116b73
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-13 19:10:42 +0200

    Added @Service to PaymentService #118

---
+++ src/main/java/com/klai/stl/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,14 @@
+package com.klai.stl.service.impl;
+
+import com.klai.stl.service.PaymentService;
+import com.klai.stl.service.reponse.CheckoutData;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Override
+    public CheckoutData checkout(String itemReference) {
+        return null;
+    }
+}
