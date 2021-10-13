9a00a2ae39cb38bd4dbbbf3d624c3d3d647f29c8
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-13 18:45:34 +0200

    Added api documentation for SubscriptionResource #118

--- src/main/java/com/klai/stl/web/rest/SubscriptionResource.java
+++ src/main/java/com/klai/stl/web/rest/SubscriptionResource.java
@@ -24,6 +24,11 @@ public class SubscriptionResource {
         this.subscriptionPlanService = subscriptionPlanService;
     }
 
+    /**
+     * Finds the subscriptions for the desired company
+     * @param companyReference the company to find the subscriptions for
+     * @return a list of subscription plans
+     */
     @GetMapping("/companies/{reference}/subscriptions")
     public ResponseEntity<List<SubscriptionPlanDTO>> findSubscriptionForCompany(@PathVariable(name = "reference") String companyReference) {
         log.debug("REST request to find company {} subscriptions", companyReference);
@@ -31,6 +36,12 @@ public class SubscriptionResource {
         return ok().body(result);
     }
 
+    /**
+     * Updates the company subscription plan for the desired company
+     * @param companyReference the company reference to update de subscription
+     * @param updateSubscriptionRequest the subscription request with the information to update
+     * @return the updated subscription plan
+     */
     @PutMapping("/companies/{reference}/subscriptions")
     public ResponseEntity<SubscriptionPlanDTO> updateSubscriptionForCompany(
         @PathVariable(name = "reference") String companyReference,
@@ -40,12 +51,21 @@ public class SubscriptionResource {
         return ok(subscriptionPlanService.updateSubscriptionPlanForCompany(companyReference, updateSubscriptionRequest));
     }
 
+    /**
+     * Find the subscriptions for the current user company
+     * @return a list of subscriptions
+     */
     @GetMapping("/company/subscriptions")
     public ResponseEntity<List<SubscriptionPlanDTO>> findSubscriptionForCurrentUserCompany() {
         log.debug("REST request to find current user company subscriptions");
         return ok().body(subscriptionPlanService.findSubscriptionsForCurrentUserCompany());
     }
 
+    /**
+     * Get the checkout information for the current user company and the subscription to buy
+     * @param reference the reference of the subscription
+     * @return the checkout data of the payment gateway
+     */
     @PostMapping("/company/subscriptions/{reference}/checkout")
     public ResponseEntity<CheckoutResponseDTO> getCheckoutForSubscription(@PathVariable String reference) {
         log.debug("REST request to get the checkout information of subscription for current user company and subscription {}", reference);
