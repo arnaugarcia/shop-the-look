f0259b9d8582805ac332192d5bf559efb46c231f
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-15 12:04:32 +0200

    Removed stripe payment gateway #118

--- src/main/java/com/klai/stl/service/PaymentGateway.java
+++
@@ -1,7 +0,0 @@
-package com.klai.stl.service;
-
-import com.klai.stl.service.reponse.GatewayProduct;
-
-public interface PaymentGateway {
-    GatewayProduct findProduct(String productReference);
-}
