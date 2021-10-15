36993214a22fa1c66f27fab8293e00210e581f8c
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-15 13:56:43 +0200

    Created and added checkout success component #118

--- src/main/webapp/app/features/company/company-routing.handler.ts
+++ src/main/webapp/app/features/company/company-routing.handler.ts
@@ -1,5 +1,5 @@
 import { AccountService } from '../../core/auth/account.service';
-import { ROUTES, Routes } from '@angular/router';
+import { Route, ROUTES, Routes } from '@angular/router';
 import { CompanyListComponent } from './pages/list/company-list.component';
 import { CompanyComponent } from './pages/company/company.component';
 import { OverviewComponent } from './pages/overview/overview.component';
@@ -15,6 +15,15 @@ import { PreferencesResolveService } from './route/preferences-resolve.service';
 import { SubscriptionRoutingResolveService } from './route/subscription-routing-resolve.service';
 import { CheckoutSuccessComponent } from './pages/checkout/checkout-success/checkout-success.component';
 import { CheckoutErrorComponent } from './pages/checkout/checkout-error/checkout-error.component';
+import { ISubscriptionPlan } from './model/subscription-plan.model';
+
+export interface RouteData {
+  subscriptions: ISubscriptionPlan[];
+}
+
+interface SubscriptionsRoute extends Route {
+  data?: RouteData;
+}
 
 @NgModule({
   imports: [CompanyModule],
@@ -55,6 +64,27 @@ export function configBookHandlerRoutes(accountService: AccountService): Routes 
   return routes;
 }
 
+const SUBSCRIPTION_ROUTES: SubscriptionsRoute[] = [
+  {
+    path: 'subscription',
+    component: SubscriptionComponent,
+    resolve: {
+      subscriptions: SubscriptionRoutingResolveService,
+    },
+  },
+  {
+    path: 'subscription/success',
+    component: CheckoutSuccessComponent,
+    resolve: {
+      subscriptions: SubscriptionRoutingResolveService,
+    },
+  },
+  {
+    path: 'subscription/error',
+    component: CheckoutErrorComponent,
+  },
+];
+
 export const COMPANY_CHILDREN_NAVIGATION = [
   {
     path: '',
@@ -74,30 +104,13 @@ export const COMPANY_CHILDREN_NAVIGATION = [
       preferences: PreferencesResolveService,
     },
   },
+  ...SUBSCRIPTION_ROUTES,
   {
     path: 'billing',
     component: BillingComponent,
     resolve: {
       billingAddress: BillingAddressRoutingResolveService,
     },
-  },
-  {
-    path: 'subscription',
-    component: SubscriptionComponent,
-    resolve: {
-      subscriptions: SubscriptionRoutingResolveService,
-    },
-  },
-  {
-    path: 'subscription/success',
-    component: CheckoutSuccessComponent,
-    resolve: {
-      subscriptions: SubscriptionRoutingResolveService,
-    },
-  },
-  {
-    path: 'subscription/error',
-    component: CheckoutErrorComponent,
   },
   {
     path: 'api-key',
