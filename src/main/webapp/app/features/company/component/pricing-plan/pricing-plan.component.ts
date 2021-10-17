fc5c9b8e63baec6e21cc20a4fe1e42de7e358df7
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-17 17:48:19 +0200

    Added specific logic for switching pricing plan for other company as admin #118

--- src/main/webapp/app/features/company/component/pricing-plan/pricing-plan.component.ts
+++ src/main/webapp/app/features/company/component/pricing-plan/pricing-plan.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ISubscriptionPlan } from '../../model/subscription-plan.model';
+import { AccountService } from '../../../../core/auth/account.service';
 
 @Component({
   selector: 'stl-pricing-plan',
@@ -12,6 +13,12 @@ export class PricingPlanComponent {
 
   @Output()
   public selectedPlan = new EventEmitter<ISubscriptionPlan>();
+
+  public isAdmin = false;
+
+  constructor(private accountService: AccountService) {
+    this.isAdmin = this.accountService.isAdmin();
+  }
 
   selectPlan(selectedPlan: ISubscriptionPlan): void {
     this.selectedPlan.emit(selectedPlan);
