2107bc4cfad0021977ebe8426ef44c1d756f5ebe
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-19 17:31:47 +0200

    Created tests for validating the structure of the StripeEvent object and the header secret signature #118

--- src/test/java/com/klai/stl/web/rest/WebhookResourceIT.java
+++ src/test/java/com/klai/stl/web/rest/WebhookResourceIT.java
@@ -2,11 +2,12 @@ package com.klai.stl.web.rest;
 
 import static com.klai.stl.web.rest.TestUtil.convertObjectToJsonBytes;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.klai.stl.IntegrationTest;
-import com.klai.stl.service.WebhookEventService;
+import com.klai.stl.domain.Company;
+import com.klai.stl.domain.SubscriptionPlan;
 import com.klai.stl.service.dto.webhook.StripeEvent;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,27 +32,53 @@ class WebhookResourceIT {
     private static final String API_URL = "/webhooks/payments/stripe";
 
     @Autowired
-    private WebhookEventService<StripeEvent> webhookEventService;
-
-    @Autowired
     private EntityManager em;
 
     @Autowired
     private MockMvc restSubscriptionMockMvc;
 
+    private Company company;
+
+    private SubscriptionPlan subscriptionPlan;
+
+    private StripeEvent stripeEvent;
+
     @BeforeEach
-    public void initTest() {}
+    public void initTest() {
+        company = CompanyResourceIT.createBasicCompany(em);
+        subscriptionPlan = SubscriptionResourceIT.createSubscriptionPlan(em);
+        stripeEvent = StripeEvent.builder().build();
+    }
 
     @Test
     @Transactional
     @WithMockUser
-    public void processEventWithInvalidHeader() throws Exception {
-        StripeEvent event = StripeEvent.builder().build();
-
+    public void processEventWithInvalidHeaderSignature() throws Exception {
         restSubscriptionMockMvc
             .perform(
-                get(API_URL).header(WEBHOOK_HEADER, "BAD_HEADER").content(convertObjectToJsonBytes(event)).contentType(APPLICATION_JSON)
+                post(API_URL)
+                    .header(WEBHOOK_HEADER, "BAD_HEADER")
+                    .content(convertObjectToJsonBytes(stripeEvent))
+                    .contentType(APPLICATION_JSON)
             )
             .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser
+    public void processEmptyEventMessageFormat() throws Exception {
+        restSubscriptionMockMvc
+            .perform(post(API_URL).header(WEBHOOK_HEADER, "BAD_HEADER").contentType(APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser
+    public void processEventWithEmptyHeaderSignature() throws Exception {
+        restSubscriptionMockMvc
+            .perform(post(API_URL).content(convertObjectToJsonBytes(stripeEvent)).contentType(APPLICATION_JSON))
+            .andExpect(status().isBadRequest());
     }
 }
