383157688086bd3a5f8ec29c417ee0af403b22f7
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-19 18:01:56 +0200

    Fixed typo #119

--- src/main/java/com/klai/stl/service/SubscriptionCheckoutService.java
+++ src/main/java/com/klai/stl/service/SubscriptionCheckoutService.java
@@ -5,7 +5,7 @@ import com.klai.stl.service.dto.CheckoutResponseDTO;
 public interface SubscriptionCheckoutService {
     /**
      * Finds and builds the data needed for checking out the subscription plan
-     * @param subscriptionPlanReference the reference of the subscription plan tu chekout
+     * @param subscriptionPlanReference the reference of the subscription plan tu checkout
      * @return the checkout response
      */
     CheckoutResponseDTO buildCheckoutDataBySubscriptionPlanForCurrentUserCompany(String subscriptionPlanReference);
