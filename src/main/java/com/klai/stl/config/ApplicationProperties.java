b86be14a17cb114c41fe4fd2891c81b7df6e9442
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-20 09:51:53 +0200

    Removed unused property

--- src/main/java/com/klai/stl/config/ApplicationProperties.java
+++ src/main/java/com/klai/stl/config/ApplicationProperties.java
@@ -13,15 +13,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
 
-    private String host;
-
     private final FeedConfiguration feed = new FeedConfiguration();
 
     private final AWSClientProperties aws = new AWSClientProperties();
 
     private final StripeClientProperties stripe = new StripeClientProperties();
-
-    public void setHost(String host) {
-        this.host = host;
-    }
 }
