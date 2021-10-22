dfc53b76ce2c84cd6a52a238c92ddae59a29954d
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-22 19:27:33 +0200

    improved messages of Stripe webhook exceptions #125

--- src/main/java/com/klai/stl/service/webhook/stripe/exception/StripeWebhookDeserialization.java
+++
@@ -1,8 +0,0 @@
-package com.klai.stl.service.webhook.stripe.exception;
-
-public class StripeWebhookDeserialization extends StripeWebhookException {
-
-    public StripeWebhookDeserialization() {
-        super("Deserialization for object failed");
-    }
-}
