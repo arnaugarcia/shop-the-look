b3bb1ce98df82c148452b2dd852d8a623fc63dea
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-24 12:36:44 +0200

    Added log when query spaces #130

--- src/main/java/com/klai/stl/web/rest/ClientResource.java
+++ src/main/java/com/klai/stl/web/rest/ClientResource.java
@@ -2,6 +2,8 @@ package com.klai.stl.web.rest;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+import com.klai.stl.service.SpaceService;
+import com.klai.stl.service.dto.SpaceDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +15,15 @@ public class ClientResource {
 
     private final Logger log = LoggerFactory.getLogger(ClientResource.class);
 
+    private final SpaceService spaceService;
+
+    public ClientResource(SpaceService spaceService) {
+        this.spaceService = spaceService;
+    }
+
     @GetMapping("/spaces/{reference}")
-    public ResponseEntity<Void> getSpaceInfo(@RequestHeader("Stripe-Signature") String headerSignature, @PathVariable String reference) {
-        return ok().build();
+    public ResponseEntity<SpaceDTO> getSpaceInfo(@RequestHeader("STL-Token") String token, @PathVariable String reference) {
+        log.info("REST request to get space ({}) data for company ({})", reference, token);
+        return ok().body(spaceService.findOne(reference));
     }
 }
