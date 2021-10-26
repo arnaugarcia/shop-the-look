aac1b19491c1428625d0a2c5ebd2e489e623920a
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-26 18:44:09 +0200

    Created ValidClientHeader annotation to handle via AOP the validation of the token #130

---
+++ src/main/java/com/klai/stl/service/client/annotation/ValidClientHeader.java
@@ -0,0 +1,13 @@
+package com.klai.stl.service.client.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface ValidClientHeader {
+}
