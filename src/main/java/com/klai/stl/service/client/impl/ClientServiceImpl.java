d94410edc461b6450ff7c1e8e4b0dff68837cd2b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-10-27 20:28:48 +0200

    Implemented logic for query space of the repository #130

--- src/main/java/com/klai/stl/service/client/impl/ClientServiceImpl.java
+++ src/main/java/com/klai/stl/service/client/impl/ClientServiceImpl.java
@@ -1,15 +1,28 @@
 package com.klai.stl.service.client.impl;
 
+import com.klai.stl.domain.Space;
+import com.klai.stl.repository.SpaceRepository;
 import com.klai.stl.service.client.ClientService;
-import com.klai.stl.service.client.dto.SpaceDTO;
+import com.klai.stl.service.client.dto.SpaceClientDTO;
+import com.klai.stl.service.client.mapper.SpaceClientMapper;
+import com.klai.stl.service.exception.SpaceNotFound;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientServiceImpl implements ClientService {
 
+    private final SpaceRepository spaceRepository;
+    private final SpaceClientMapper spaceClientMapper;
+
+    public ClientServiceImpl(SpaceRepository spaceRepository, SpaceClientMapper spaceClientMapper) {
+        this.spaceRepository = spaceRepository;
+        this.spaceClientMapper = spaceClientMapper;
+    }
+
     // Apply cache here
     @Override
-    public SpaceDTO findByReference(String reference) {
-        return null;
+    public SpaceClientDTO findByReference(String reference) {
+        final Space space = spaceRepository.findByReferenceWithEagerRelationships(reference).orElseThrow(SpaceNotFound::new);
+        return spaceClientMapper.map(space);
     }
 }
