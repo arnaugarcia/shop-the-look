de03b698240c6a14e1368194ae26395a09696b10
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-11-02 19:20:51 +0100

    Implemented authentication on Client request #130

--- src/main/java/com/klai/stl/security/token/ClientTokenFilter.java
+++ src/main/java/com/klai/stl/security/token/ClientTokenFilter.java
@@ -3,11 +3,15 @@ package com.klai.stl.security.token;
 import static org.springframework.util.StringUtils.hasText;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class ClientTokenFilter extends GenericFilterBean {
@@ -19,10 +23,10 @@ public class ClientTokenFilter extends GenericFilterBean {
         throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String token = extractToken(httpServletRequest);
-        /*if (hasText(token)) {
-            Authentication authentication = new AnonymousAuthenticationToken("key", new User(), null);
+        if (hasText(token)) {
+            Authentication authentication = new UsernamePasswordAuthenticationToken(null, token, new ArrayList<>());
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        }*/
+        }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
