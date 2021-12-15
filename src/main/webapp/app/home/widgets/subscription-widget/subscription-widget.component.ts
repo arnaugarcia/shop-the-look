012d7b15f13cfd38b5fd861f684ac11fbb9603a5
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-12-15 20:34:00 +0100

    Re-styled the subscription widget component #143

--- src/main/webapp/app/home/widgets/subscription-widget/subscription-widget.component.ts
+++ src/main/webapp/app/home/widgets/subscription-widget/subscription-widget.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Account } from '../../../core/auth/account.model';
 import { StatisticsService } from '../../services/statistics.service';
 import { SubscriptionStatus } from '../../models/statistics.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'stl-subscription-widget',
@@ -14,11 +15,15 @@ export class SubscriptionWidgetComponent implements OnInit {
 
   public subscriptionStatus?: SubscriptionStatus;
 
-  constructor(private statisticsService: StatisticsService) {}
+  constructor(private statisticsService: StatisticsService, private router: Router) {}
 
   ngOnInit(): void {
     this.statisticsService.querySubscriptionStats().subscribe(response => {
       this.subscriptionStatus = response.body!;
     });
+  }
+
+  navigateSubscriptionPage(): void {
+    this.router.navigate(['company', 'subscription']);
   }
 }
