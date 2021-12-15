a62bf36e7a188b33d35010d32eef4237184d7ac7
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-12-15 22:11:00 +0100

    Fixed static use of lombok builder of SpaceCriteria in Statistics service and impl

--- src/main/java/com/klai/stl/service/space/criteria/SpaceCriteria.java
+++ src/main/java/com/klai/stl/service/space/criteria/SpaceCriteria.java
@@ -10,8 +10,4 @@ public class SpaceCriteria implements Serializable {
 
     String keyword;
     String companyReference;
-
-    public static SpaceCriteria.SpaceCriteriaBuilder from(SpaceCriteriaDTO spaceCriteriaDTO) {
-        return SpaceCriteria.builder().keyword(spaceCriteriaDTO.getKeyword());
-    }
 }
