f6dbae3cb00060e6ac2dc0c8386beb17bbc9a547
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-05-16 17:07:02 +0200

    Added event criteria validation

--- src/main/java/com/klai/stl/service/event/criteria/EventCriteria.java
+++ src/main/java/com/klai/stl/service/event/criteria/EventCriteria.java
@@ -3,15 +3,23 @@ package com.klai.stl.service.event.criteria;
 import com.klai.stl.service.event.dto.WebEventType;
 import java.time.ZonedDateTime;
 import java.util.Map;
+import javax.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value
 public class EventCriteria {
 
+    @NotNull
     String company;
+
     String space;
+
+    @NotNull
     WebEventType type;
+
     ZonedDateTime startDate;
+
     ZonedDateTime endDate;
+
     Map<String, String> sort;
 }
