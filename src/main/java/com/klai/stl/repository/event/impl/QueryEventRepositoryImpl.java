927c04b4c3f5ede60c7656b74f991fd4d5c5cedc
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-05-16 17:52:02 +0200

    Indented file

--- src/main/java/com/klai/stl/repository/event/impl/QueryEventRepositoryImpl.java
+++ src/main/java/com/klai/stl/repository/event/impl/QueryEventRepositoryImpl.java
@@ -1,5 +1,21 @@
 package com.klai.stl.repository.event.impl;
 
+import com.klai.stl.domain.event.Event;
+import com.klai.stl.repository.event.QueryEventOperations;
+import com.klai.stl.repository.event.QueryEventRepository;
+import com.klai.stl.repository.event.dto.EventValue;
+import com.klai.stl.repository.event.dto.Timeline;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.Query;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.klai.stl.domain.event.Event.SPACE_KEYWORD;
 import static com.klai.stl.service.event.dto.WebEventType.SPACE_VIEW;
 import static java.util.Optional.ofNullable;
@@ -7,21 +23,6 @@ import static java.util.stream.Collectors.toList;
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static org.elasticsearch.search.aggregations.AggregationBuilders.dateHistogram;
 import static org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval.DAY;
-
-import com.klai.stl.domain.event.Event;
-import com.klai.stl.repository.event.QueryEventOperations;
-import com.klai.stl.repository.event.QueryEventRepository;
-import com.klai.stl.repository.event.dto.EventValue;
-import com.klai.stl.repository.event.dto.Timeline;
-import java.util.ArrayList;
-import java.util.List;
-import javax.validation.constraints.NotNull;
-import org.elasticsearch.search.aggregations.Aggregations;
-import org.elasticsearch.search.aggregations.bucket.terms.Terms;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.Query;
 
 public class QueryEventRepositoryImpl implements QueryEventRepository, QueryEventOperations {
 
@@ -34,7 +35,9 @@ public class QueryEventRepositoryImpl implements QueryEventRepository, QueryEven
     @Override
     public List<EventValue> findSpaceViewsByCompany(@NotNull String companyReference) {
         Query query = new NativeSearchQueryBuilder()
-            .withQuery(boolQuery().filter(byCompany(companyReference)).filter(byType(SPACE_VIEW)))
+            .withQuery(boolQuery()
+                .filter(byCompany(companyReference))
+                .filter(byType(SPACE_VIEW)))
             .addAggregation(groupBySpace())
             .build();
 
@@ -46,21 +49,28 @@ public class QueryEventRepositoryImpl implements QueryEventRepository, QueryEven
     @Override
     public List<Timeline> findSpaceViewsByCompanyAndDateRange(String companyReference, String startDate, String endDate) {
         Query query = new NativeSearchQueryBuilder()
-            .withQuery(
-                boolQuery().filter(byCompany(companyReference)).filter(byType(SPACE_VIEW)).filter(byTimestampBetween(startDate, endDate))
-            )
-            .addAggregation(groupBySpace().subAggregation(dateHistogram("space_timeline").field("timestamp").fixedInterval(DAY)))
-            .build();
+            .withQuery(boolQuery()
+                .filter(byCompany(companyReference))
+                .filter(byType(SPACE_VIEW))
+                .filter(byTimestampBetween(startDate, endDate)))
+            .addAggregation(groupBySpace()
+                .subAggregation(dateHistogram("space_timeline")
+                    .field("timestamp")
+                    .fixedInterval(DAY))).build();
 
         final SearchHits<Event> search = elasticsearchOperations.search(query, Event.class);
         return new ArrayList<>();
     }
 
     private List<EventValue> buildEventValueFrom(Terms terms) {
-        return terms.getBuckets().stream().map(EventValue::new).collect(toList());
+        return terms.getBuckets()
+            .stream()
+            .map(EventValue::new)
+            .collect(toList());
     }
 
     private Aggregations getAggregationsOf(SearchHits<Event> search) {
-        return ofNullable(search.getAggregations()).orElseThrow(() -> new IllegalStateException("Aggregations are not found"));
+        return ofNullable(search.getAggregations())
+            .orElseThrow(() -> new IllegalStateException("Aggregations are not found"));
     }
 }
