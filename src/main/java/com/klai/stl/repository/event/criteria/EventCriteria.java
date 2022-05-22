28b34b9477f09c2a1b7482f20fd39ee8c83dddfa
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-05-22 18:50:36 +0200

    Extended Event criteria builder and added a constraint if the startDate is superior to the end date

--- src/main/java/com/klai/stl/repository/event/criteria/EventCriteria.java
+++ src/main/java/com/klai/stl/repository/event/criteria/EventCriteria.java
@@ -1,9 +1,13 @@
 package com.klai.stl.repository.event.criteria;
 
+import static com.klai.stl.repository.event.criteria.EventCriteria.CriteriaSort.ASC;
+import static com.klai.stl.repository.event.criteria.EventCriteria.CriteriaSort.DESC;
 import static java.time.Instant.now;
 import static java.util.Objects.requireNonNull;
+import static org.elasticsearch.search.aggregations.BucketOrder.key;
 
 import javax.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Value;
 import org.elasticsearch.search.aggregations.BucketOrder;
 
@@ -12,16 +16,33 @@ public class EventCriteria {
 
     String company;
     CriteriaSort sort;
-
     Long startDate;
-
     Long endDate;
 
-    public EventCriteria(@NotNull String company, CriteriaSort sort, Long startDate, Long endDate) {
+    @Builder(builderMethodName = "hiddenBuilder")
+    private EventCriteria(@NotNull String company, CriteriaSort sort, Long startDate, Long endDate) {
         this.company = requireNonNull(company);
-        this.sort = sort == null ? CriteriaSort.DESC : sort;
+        this.sort = sort == null ? DESC : sort;
         this.startDate = startDate;
         this.endDate = endDate == null ? now().toEpochMilli() : endDate;
+        if (startDate != null && endDate != null && startDate > endDate) {
+            throw new IllegalArgumentException("startDate must be less than endDate");
+        }
+    }
+
+    public static EventCriteriaBuilder builder(String company) {
+        return new EventCriteriaBuilder().company(company);
+    }
+
+    public static class EventCriteriaBuilder {
+
+        public EventCriteriaBuilder sortAsc() {
+            return this.sort(ASC);
+        }
+
+        public EventCriteriaBuilder sortDesc() {
+            return this.sort(DESC);
+        }
     }
 
     public enum CriteriaSort {
@@ -30,6 +51,6 @@ public class EventCriteria {
     }
 
     public BucketOrder bucketOrder() {
-        return sort == CriteriaSort.ASC ? BucketOrder.key(true) : BucketOrder.key(false);
+        return sort == ASC ? key(true) : key(false);
     }
 }
