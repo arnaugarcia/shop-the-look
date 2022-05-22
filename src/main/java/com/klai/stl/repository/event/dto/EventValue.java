1505c357c277bfe0a2d04a74c6f7417bb0a3ac98
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-05-22 16:28:26 +0200

    Added annotations to event value to require that the object is not null

--- src/main/java/com/klai/stl/repository/event/dto/EventValue.java
+++ src/main/java/com/klai/stl/repository/event/dto/EventValue.java
@@ -1,5 +1,8 @@
 package com.klai.stl.repository.event.dto;
 
+import static java.util.Objects.requireNonNull;
+
+import javax.validation.constraints.NotNull;
 import lombok.Value;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket;
 import org.elasticsearch.search.aggregations.metrics.NumericMetricsAggregation;
@@ -10,17 +13,19 @@ public class EventValue {
     String key;
     String value;
 
-    public EventValue(String key, String value) {
-        this.key = key;
-        this.value = value;
+    public EventValue(@NotNull String key, @NotNull String value) {
+        this.key = requireNonNull(key);
+        this.value = requireNonNull(value);
     }
 
-    public EventValue(Bucket bucket) {
+    public EventValue(@NotNull Bucket bucket) {
+        requireNonNull(bucket);
         this.key = bucket.getKeyAsString();
         this.value = String.valueOf(bucket.getDocCount());
     }
 
-    public EventValue(NumericMetricsAggregation.SingleValue terms) {
+    public EventValue(@NotNull NumericMetricsAggregation.SingleValue terms) {
+        requireNonNull(terms);
         this.key = terms.getName();
         this.value = terms.getValueAsString();
     }
