45c2ca6805d665f892c38078e65a29c01fd16d3b
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-05-31 19:25:07 +0200

    Created analytics service and implemented method to find space views

---
+++ src/main/webapp/app/features/analytics/services/analytics.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { ApplicationConfigService } from '../../../core/config/application-config.service';
+import { Observable } from 'rxjs';
+import { ISpaceReport } from '../models/space-report.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnalyticsService {
+  protected resourceUrl = this.applicationConfigService.getEndpointFor('api/analytics');
+
+  constructor(private http: HttpClient, private applicationConfigService: ApplicationConfigService) {}
+
+  findSpaceViews(): Observable<HttpResponse<ISpaceReport[]>> {
+    return this.http.get<ISpaceReport[]>(`${this.resourceUrl}/spaces/views`, { observe: 'response' });
+  }
+}
