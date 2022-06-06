fba5c253a0ecaf03d7366d92ec361323c363278e
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-06-06 12:33:54 +0200

    Displaying views in the chart

--- src/main/webapp/app/features/analytics/pages/analytics/widgets/spaces-view-clicks/spaces-view-clicks.component.ts
+++ src/main/webapp/app/features/analytics/pages/analytics/widgets/spaces-view-clicks/spaces-view-clicks.component.ts
@@ -35,25 +35,10 @@ export type ChartOptions = {
   styleUrls: ['./spaces-view-clicks.component.scss'],
 })
 export class SpacesViewClicksComponent implements OnInit {
-  public spaceViews: ISpaceReport[] = [];
-  public spaceClicks: ISpaceReport[] = [];
-
   @ViewChild('chart') chart: ChartComponent | undefined;
+
   public chartOptions: Partial<ChartOptions> | any = {
-    series: [
-      {
-        name: 'Net Profit',
-        data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-      },
-      {
-        name: 'Revenue',
-        data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-      },
-      {
-        name: 'Free Cash Flow',
-        data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
-      },
-    ],
+    series: [],
     chart: {
       type: 'bar',
       height: 350,
@@ -65,31 +50,16 @@ export class SpacesViewClicksComponent implements OnInit {
         borderRadius: 2,
       },
     },
-    dataLabels: {
-      enabled: false,
-    },
     stroke: {
       show: true,
       width: 2,
       colors: ['transparent'],
     },
     xaxis: {
-      categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-    },
-    yaxis: {
-      title: {
-        text: '$ (thousands)',
-      },
+      type: 'category',
     },
     fill: {
       opacity: 1,
-    },
-    tooltip: {
-      y: {
-        formatter: function (val: string) {
-          return '$ ' + val + ' thousands';
-        },
-      },
     },
   };
 
@@ -98,17 +68,15 @@ export class SpacesViewClicksComponent implements OnInit {
   ngOnInit(): void {
     this.analyticsService.findSpaceViews().subscribe((response: HttpResponse<ISpaceReport[]>) => {
       if (response.body) {
-        this.spaceViews = response.body;
+        this.chart?.updateSeries([
+          {
+            data: response.body.map(spaceReport => ({
+              x: spaceReport.reference ? spaceReport.reference : 'Deleted space',
+              y: spaceReport.value,
+            })),
+          },
+        ]);
       }
     });
-    this.analyticsService.findSpaceClicks().subscribe((response: HttpResponse<ISpaceReport[]>) => {
-      if (response.body) {
-        this.spaceClicks = response.body;
-      }
-    });
-  }
-
-  emittedEvents($event: any): void {
-    console.error($event);
   }
 }
