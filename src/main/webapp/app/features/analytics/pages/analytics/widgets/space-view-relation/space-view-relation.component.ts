fb63d49b8c0bd35b684d26f96dec22f1352ffe79
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-06-11 13:05:47 +0200

    Added logic to query space view relation data

--- src/main/webapp/app/features/analytics/pages/analytics/widgets/space-view-relation/space-view-relation.component.ts
+++ src/main/webapp/app/features/analytics/pages/analytics/widgets/space-view-relation/space-view-relation.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AnalyticsWidgetComponent } from '../widget';
+import { AnalyticsService } from '../../../../services/analytics.service';
+import { IAnalyticsCriteria } from '../../../../models/analytics-criteria.model';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'stl-space-view-relation',
   templateUrl: './space-view-relation.component.html',
-  styleUrls: ['./space-view-relation.component.scss'],
 })
-export class SpaceViewRelationComponent implements OnInit {
-  constructor() {}
+export class SpaceViewRelationComponent extends AnalyticsWidgetComponent {
+  constructor(analyticsService: AnalyticsService) {
+    super(analyticsService);
+  }
 
-  ngOnInit(): void {}
+  loadAll(criteria?: IAnalyticsCriteria): void {
+    this.analyticsService.findSpaceViewRelation(criteria).subscribe((response: HttpResponse<any>) => {
+      console.error(response.body);
+    });
+  }
 }
