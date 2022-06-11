30ec3bb0fd6819f64bf9926771b46fc2ae9e5f74
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-06-11 18:55:36 +0200

    Created top product widget

---
+++ src/main/webapp/app/home/widgets/top-product/top-product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from '../../../features/analytics/services/analytics.service';
+import { HttpResponse } from '@angular/common/http';
+import { IProductReport } from '../../../features/analytics/models/product-report.model';
+
+@Component({
+  selector: 'stl-top-product',
+  templateUrl: './top-product.component.html',
+  styleUrls: ['./top-product.component.scss'],
+})
+export class TopProductComponent implements OnInit {
+  public product?: IProductReport;
+
+  constructor(private analyticsService: AnalyticsService) {}
+
+  ngOnInit(): void {
+    this.analyticsService.findProductClicks({ limit: 1 }).subscribe((response: HttpResponse<IProductReport[]>) => {
+      if (response.body) {
+        this.product = response.body[0];
+      }
+    });
+  }
+}
