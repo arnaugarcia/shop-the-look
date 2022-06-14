f18c70f1fdd81824d233ae1036864dc0cc2cc8bd
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-06-14 18:47:58 +0200

    Added missing nullish values for product report model

--- src/main/webapp/app/features/analytics/models/product-report.model.ts
+++ src/main/webapp/app/features/analytics/models/product-report.model.ts
@@ -1,8 +1,8 @@
 export interface IProductReport {
-  name: string;
-  price: string;
-  sku: string;
-  link: string;
-  reference: string;
+  name?: string;
+  price?: string;
+  sku?: string;
+  link?: string;
+  reference?: string;
   value: number;
 }
